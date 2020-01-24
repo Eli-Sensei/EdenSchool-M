5afa7337c069dc3da2cedaa0b67e0ccb1d5ce1b1
Ellyas-San <mepereiradebarros@edenschool.fr>
2020-01-24 14:08:59 +0100

    Insertion du TP

---
+++ TP/TP Selecteur/script.js
@@ -0,0 +1,11 @@
+Louis
+
+UP;
+Right(6);
+TakeMagasine;
+RIGHT(1.5);
+LayOnEtagere(Magasine);
+BACK(1.5);
+LEFT(6);
+LookRight;
+DOWN;
---
+++ TP/TP1/code.js
@@ -0,0 +1,2 @@
+document.write("HELLO WORLD")
+document.write(alert("STOP !!!!"))
